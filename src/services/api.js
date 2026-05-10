ac01b2d2559bfc9d6b8e9f66bd679702352640a6
1pradeep1 <pradeep.c.0532@gmail.com>
2026-05-10 11:37:52 +0530

    Update API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://taskflow-backend-cmzo.onrender.com/api',
 });
-
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
